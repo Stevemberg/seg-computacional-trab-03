5b0cd44bb9e0b2cc43c9ff61a35d4476c8621481
stevemberg <stevemberg100@gmail.com>
2023-12-20 03:15:44 -0300

    remove files

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		RSA rsa = new RSA();
-		Util u = new Util();
 		ArrayList<BigInteger> array;
 
 		String msg = "Stevemberg12@#";
